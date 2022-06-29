5df7f6111d072b196007c02d7306299d5058763a
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-06-29 23:51:50 +0900

    상속관계 매핑 실전예제

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
